3553afcd4cd64768700a33deb200b87dc3cdd905
nik86olay <nik86olaygmail.com>
2023-03-16 22:17:15 +0300

    Initial commit

---
+++ src/main/java/org/example/IntsCalculator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result.intValue();
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result.intValue();
+    }
+
+    @Override
+    public int pow(int arg0, int arg1) {
+        return target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.POW).result.intValue();
+    }
+}
